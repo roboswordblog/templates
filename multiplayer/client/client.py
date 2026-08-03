8f1009830ba0edaed3e4adad49a8db720f21772c
Robosword <robotanmol@gmail.com>
2026-08-03 01:13:38 -0700

    Implement ClientHandler class for socket connection

---
+++ multiplayer/client/client.py
@@ -0,0 +1,11 @@
+import socket
+
+class ClientHandler:
+    def __init__(self, port):
+        self.FORMAT = 'utf-8'
+        self.DISCONNECT_MESSAGE = '!DISCONNECT'
+        self.PORT = port
+        self.SERVER = ""
+        self.ADDR = (self.SERVER, self.PORT)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(self.addr)
